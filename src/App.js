997c4a5aac619101be1096b5c741cbb43ff885a6
권수연 <yeoniii20@epozen.com>
2023-04-25 16:21:20 +0900

    fix: session FlexTest만 보이도록 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import FelxTest4 from "./constant/flexTest4";
+import FlexTest5 from "./constant/flexTest5";
 
 function App() {
   return (
     <div className="App">
-      <FelxTest4 />
+      {/* <FelxTest4 /> */}
+      <FlexTest5 />
     </div>
   );
 }
