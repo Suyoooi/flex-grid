39e3882232f675103b6b9d0fa308b63b58ce95f0
권수연 <yeoniii20@epozen.com>
2023-04-25 15:40:13 +0900

    fix: 불필요한 코드 삭제

--- src/constant/flexTest4.jsx
+++ src/constant/flexTest4.jsx
@@ -13,14 +13,6 @@ export default function FelxTest4() {
     { i: "b", x: 0, y: 1, w: 5, h: 9 },
     { i: "c", x: 5, y: 0, w: 6, h: 9 },
     { i: "d", x: 5, y: 2, w: 5, h: 9 },
-    // { i: "e", x: 0, y: 2, w: 1, h: 2 },
-    // { i: "f", x: 1, y: 2, w: 4, h: 2 },
-    // { i: "g", x: 4, y: 0, w: 1, h: 2 },
-    // { i: "h", x: 1, y: 0, w: 3, h: 5 },
-    // { i: "i", x: 4, y: 0, w: 1, h: 3 },
-    // { i: "j", x: 0, y: 2, w: 1, h: 2 },
-    // { i: "k", x: 0, y: 2, w: 1, h: 4 },
-    // { i: "l", x: 1, y: 2, w: 4, h: 3 },
   ]);
 
   useEffect(() => {
@@ -34,8 +26,6 @@ export default function FelxTest4() {
         layout={layout}
         measureBeforeMount={false}
         useCSSTransforms={mounted}
-        compactType={compactType}
-        preventCollision={!compactType}
         isDroppable={true}
         droppingItem={{ i: "xx", h: 50, w: 250 }}
       >
