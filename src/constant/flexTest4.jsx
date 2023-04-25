502728904f61114917a9dd742148380e36ce28b2
권수연 <yeoniii20@epozen.com>
2023-04-25 15:37:56 +0900

    fix: 불필요한 코드 삭제

--- src/constant/flexTest4.jsx
+++ src/constant/flexTest4.jsx
@@ -9,68 +9,29 @@ export default function FelxTest4() {
   const [compactType, setcompactType] = useState("vertical");
   const [mounted, setmounted] = useState(false);
   const [layout, setlayout] = useState([
-    { i: "a", x: 0, y: 0, w: 1, h: 2 },
-    { i: "b", x: 1, y: 0, w: 3, h: 2 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 },
-    { i: "d", x: 0, y: 2, w: 1, h: 2 },
-    { i: "e", x: 0, y: 2, w: 1, h: 2 },
-    { i: "f", x: 1, y: 2, w: 4, h: 2 },
-    { i: "g", x: 4, y: 0, w: 1, h: 2 },
-    { i: "h", x: 1, y: 0, w: 3, h: 5 },
-    { i: "i", x: 4, y: 0, w: 1, h: 3 },
-    { i: "j", x: 0, y: 2, w: 1, h: 2 },
-    { i: "k", x: 0, y: 2, w: 1, h: 4 },
-    { i: "l", x: 1, y: 2, w: 4, h: 3 },
+    { i: "a", x: 0, y: 0, w: 5, h: 9 },
+    { i: "b", x: 0, y: 1, w: 5, h: 9 },
+    { i: "c", x: 5, y: 0, w: 6, h: 9 },
+    { i: "d", x: 5, y: 2, w: 5, h: 9 },
+    // { i: "e", x: 0, y: 2, w: 1, h: 2 },
+    // { i: "f", x: 1, y: 2, w: 4, h: 2 },
+    // { i: "g", x: 4, y: 0, w: 1, h: 2 },
+    // { i: "h", x: 1, y: 0, w: 3, h: 5 },
+    // { i: "i", x: 4, y: 0, w: 1, h: 3 },
+    // { i: "j", x: 0, y: 2, w: 1, h: 2 },
+    // { i: "k", x: 0, y: 2, w: 1, h: 4 },
+    // { i: "l", x: 1, y: 2, w: 4, h: 3 },
   ]);
 
   useEffect(() => {
     setmounted(true);
   }, []);
 
-  const onCompactTypeChange = () => {
-    const oldCompactType = compactType;
-    const compactType =
-      oldCompactType === "horizontal"
-        ? "vertical"
-        : oldCompactType === "vertical"
-        ? null
-        : "horizontal";
-    setcompactType(compactType);
-  };
-
-  const onDrop = (elemParams) => {
-    alert(
-      `Element parameters:\n${JSON.stringify(
-        elemParams,
-        ["x", "y", "w", "h"],
-        2
-      )}`
-    );
-  };
-
   return (
     <div>
-      <div
-        className="droppable-element"
-        style={{
-          height: "50",
-          width: "250",
-          backgroundColo: "pink",
-          margin: "10",
-          border: "2",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        draggable={true}
-        unselectable="on"
-        onDragStart={(e) => e.dataTransfer.setData("text/plain", "")}
-      ></div>
       <ResponsiveReactGridLayout
         rowHeight={30}
-        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
         layout={layout}
-        onDrop={onDrop}
         measureBeforeMount={false}
         useCSSTransforms={mounted}
         compactType={compactType}
