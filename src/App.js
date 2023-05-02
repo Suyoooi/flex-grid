387bb53149e07e94d332dd8ab72cbc715d88d7df
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-05-02 11:30:17 +0900

    feat: grid에 react-chart 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import FelxTest4 from "./constant/flexTest4";
 import FlexTest5 from "./constant/flexTest5";
+import ReactChartTest from "./constant/react-chart";
 
 function App() {
   return (
     <div className="App">
       {/* <FelxTest4 /> */}
       <FlexTest5 />
+      {/* <ReactChartTest /> */}
     </div>
   );
 }
